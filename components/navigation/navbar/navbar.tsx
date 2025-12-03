3fa9e87531e21220cb5e6ced6aed0acd09695e4a
saulprl <saul.ramos@necodex.com>
2025-12-02 23:35:25 -0700

    wip: navbar with sidebar menu

--- components/navigation/navbar/navbar.tsx
+++ components/navigation/navbar/navbar.tsx
@@ -1,35 +1,89 @@
 import { Logo } from "@/components/logo/logo";
+import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuList,
+  NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { Menu, Search, ShoppingBag, UserRound } from "lucide-react";
 import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className="flex w-full items-center justify-between gap-8 bg-neutral-50 px-4 py-4">
-      <Link href="/">
-        <Logo />
-      </Link>
+    <header className="flex w-full flex-col">
+      <div className="flex w-full items-center justify-between gap-8 bg-neutral-50 px-4 py-4">
+        <Link href="/">
+          <Logo />
+        </Link>
 
-      <NavigationMenu>
-        <NavigationMenuList className="gap-6">
-          <NavigationMenuItem>
-            <Search />
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <ShoppingBag />
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <UserRound />
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Menu />
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+        <NavigationMenu>
+          <NavigationMenuList className="gap-2 sm:gap-6">
+            <NavigationMenuItem asChild>
+              <Button variant="ghost" size="icon-sm">
+                <Search />
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem asChild>
+              <Button variant="ghost" size="icon-sm">
+                <ShoppingBag />
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem asChild>
+              <Button variant="ghost" size="icon-sm">
+                <UserRound />
+              </Button>
+            </NavigationMenuItem>
+            <SidebarProvider>
+              <NavigationMenuItem asChild className="md:hidden">
+                <SidebarTrigger asChild>
+                  <Button variant="ghost" size="icon-sm">
+                    <Menu />
+                  </Button>
+                </SidebarTrigger>
+              </NavigationMenuItem>
+              <Sidebar side="right" variant="inset">
+                <SidebarContent>
+                  <SidebarInset>
+                    <NavigationMenu className="w-full bg-red-500">
+                      <NavigationMenuList className="flex w-full flex-col">
+                        <NavigationMenuItem>
+                          <NavigationMenuTrigger>Tops</NavigationMenuTrigger>
+                          <NavigationMenuContent>
+                            <ul className="grid w-full grid-cols-2 gap-2">
+                              <li>Sweaters</li>
+                              <li>Coats</li>
+                              <li>Henleys</li>
+                              <li>Cardigans</li>
+                              <li>Blazers</li>
+                              <li>Polo Shirts</li>
+                            </ul>
+                          </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>Bottoms</NavigationMenuItem>
+                        <NavigationMenuItem>All</NavigationMenuItem>
+                        <NavigationMenuItem>
+                          Personalized Clothing
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>Pre-Orders</NavigationMenuItem>
+                        <NavigationMenuItem>About Us</NavigationMenuItem>
+                      </NavigationMenuList>
+                    </NavigationMenu>
+                  </SidebarInset>
+                </SidebarContent>
+              </Sidebar>
+            </SidebarProvider>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </header>
   );
 };
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -29,7 +29,7 @@ const buttonVariants = cva(
         sm: "rounded-full gap-1.5 px-6 py-2 has-[>svg]:px-2.5 not-[has(>svg)]:px-6 text-base",
         lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
         icon: "size-9",
-        "icon-sm": "size-8",
+        "icon-sm": "size-8 [&_svg:not([class*='size-'])]:size-auto",
         "icon-lg": "size-10",
         "filter-sm": "min-w-12 h-12 p-2 rounded-md border-neutral-20 text-2xl",
         "filter-lg":
