a12058bebff6857ef61f4e7e1ac8ec11ff97bb5c
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-12-16 08:48:11 -0700

    changes

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -18,24 +18,23 @@ export function GridTileImage({
   };
 } & React.ComponentProps<typeof Image>) {
   return (
-    <div
-      className={clsx(
-        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black',
-        {
-          relative: label,
-          'border-2 border-blue-600': active,
-          'border-neutral-200 dark:border-neutral-800': !active
-        }
-      )}
-    >
-      {props.src ? (
-        <Image
-          className={clsx('relative h-full w-full object-contain', {
-            'transition duration-300 ease-in-out group-hover:scale-105': isInteractive
-          })}
-          {...props}
-        />
-      ) : null}
+    <div className="flex h-full w-full flex-col gap-3">
+      <div
+        className={clsx(
+          'group relative aspect-square w-full overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black',
+          {
+            'border-2 border-blue-600': active,
+            'border-neutral-200 dark:border-neutral-800': !active
+          }
+        )}
+      >
+        {props.src ? (
+          <Image
+            className="h-full w-full object-cover"
+            {...props}
+          />
+        ) : null}
+      </div>
       {label ? (
         <Label
           title={label.title}
