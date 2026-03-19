03e9caa3cc1079ca534e508875c0917d032b97e4
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2026-03-18 21:38:28 -0700

    reducir tama de bag icon

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -36,10 +36,10 @@ export function GridTileImage({
             {...props}
           />
         ) : null}
-        <div className="absolute right-2 bottom-2 flex h-7 w-7 items-center justify-center rounded-full bg-white shadow-md transition-all duration-200 hover:scale-110 sm:right-3 sm:bottom-3 sm:h-9 sm:w-9 dark:bg-neutral-900">
+        <div className="absolute right-2 bottom-2 flex h-5 w-5 items-center justify-center rounded-full bg-white shadow-md transition-all duration-200 hover:scale-110 sm:right-3 sm:bottom-3 sm:h-9 sm:w-9 dark:bg-neutral-900">
           <ShoppingBag
             suppressHydrationWarning
-            className="h-3.5 w-3.5 text-neutral-700 sm:h-4 sm:w-4 dark:text-neutral-300"
+            className="h-2.5 w-2.5 text-neutral-700 sm:h-4 sm:w-4 dark:text-neutral-300"
           />
         </div>
       </div>
--- components/label.tsx
+++ components/label.tsx
@@ -1,23 +1,23 @@
-import Price from './price';
+import Price from "./price";
 
 const Label = ({
   title,
   amount,
   currencyCode,
-  position = 'bottom'
+  position = "bottom",
 }: {
   title: string;
   amount: string;
   currencyCode: string;
-  position?: 'bottom' | 'center';
+  position?: "bottom" | "center";
 }) => {
   return (
     <div className="flex w-full flex-col gap-0.5 sm:gap-1">
-      <h3 className="line-clamp-2 text-[10px] font-semibold leading-[1.2] text-black sm:text-xs dark:text-white">
+      <h3 className="line-clamp-2 text-[10px] leading-[1.2] font-semibold text-black sm:text-xs dark:text-white">
         {title}
       </h3>
       <Price
-        className="text-[10px] font-normal leading-[1.2] text-black/95 sm:text-xs dark:text-white/95"
+        className="text-[10px] leading-[1.2] font-normal text-black/95 sm:text-xs dark:text-white/95"
         currencyCodeClassName="text-black/70 dark:text-white/70"
         amount={amount}
         currencyCode={currencyCode}
