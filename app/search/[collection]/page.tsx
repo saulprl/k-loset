d11bef367ab2bdab0c51ee3627996fbdb94a3564
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2026-03-19 22:44:09 -0700

    cambios

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -43,7 +43,7 @@ export default async function CategoryPage(props: {
       {products.length === 0 ? (
         <p className="py-3 text-lg">{`No products found in this collection`}</p>
       ) : (
-        <Grid className="grid-cols-3 gap-3 sm:gap-5 lg:grid-cols-3">
+        <Grid className="grid-cols-2 gap-3 sm:grid-cols-3 sm:gap-5 lg:grid-cols-3">
           <ProductGridItems products={products} />
         </Grid>
       )}
